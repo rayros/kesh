d61f8dc2177bda597aabab10b1e0926366f5a069
Paweł Łaski <pawel.laski@bacca.pl>
2024-10-08 00:54:32 +0200

    Added fifo to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
+pub mod fifo;
 pub mod fifo_reinserion;
 
+pub use fifo::FIFO;
 pub use fifo_reinserion::FIFOReinsertion;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn fifo_works() {
+        let mut cache = FIFO::new(10);
+        cache.put(1, 1, 2).unwrap();
+        assert_eq!(cache.get(1), Some(&1));
+        assert_eq!(cache.get(2), None);
+    }
 
     #[test]
     fn fifo_reinserion() {
